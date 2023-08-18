43b8b2fa691c47b6e0130634b029ab08fb81bdf8
Terence An <terence@itos.fi>
2023-08-18 14:21:56 -0400

    Licensing

--- src/interfaces/IWETH.sol
+++ src/interfaces/IWETH.sol
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 
 interface IWETH {
     function deposit() external payable;
-    function withdraw(uint wad) external;
+    function withdraw(uint256 wad) external;
 }
